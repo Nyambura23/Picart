34e205cfca0bc37a5130a0837c65c7fee09a0aea
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-06-02 06:43:58 +0300

    add date

--- pics/urls.py
+++ pics/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns=[
     path('',views.welcome,name = 'welcome'),
+    path('today',views.news_of_day,name='newsToday')
 ]
--- pics/views.py
+++ pics/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime as dte
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to Picsart')
+
+def news_of_day(request):
+    date = dte.date.today()
+
+        # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
+
+
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dte.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+
