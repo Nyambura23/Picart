40ca037dd9ba5402bcf5e42768af896ceb694a68
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-06-04 02:15:24 +0300

    update a route to display single images

--- pics/tests.py
+++ pics/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import User,tags
+from .models import User,Location, Image, tags
 
 
 class UserTestClass(TestCase):
@@ -18,3 +18,54 @@ class UserTestClass(TestCase):
         self.miss.save_user()
         users = User.objects.all()
         self.assertTrue(len(users) > 0)
+
+class ImageTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.Bee= Image(id = '1', image = 'example.jpg', name = 'Bee', description ='Just one of my favourite photos', location ='Nairobi', tags ='Portrait')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Bee,Image)) 
+    
+     # Testing Save Method
+    def test_save_method(self):
+        self.Bee.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)  
+
+class LocationTestClass(TestCase):       
+      # Set up method
+    def setUp(self):
+        self.Nairobi= Location(location='Nairobi')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Nairobi,Location))    
+        
+    def test_save_method(self):
+        self.Nairobi.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)>0)    
+        
+    def test_delete_method(self):
+        self.Nairobi.delete_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)==0)
+
+class tagsTestClass(TestCase):       
+      # Set up method
+    def setUp(self):
+        self.Portrait= tags(tag='Portrait')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Portrait,tags))    
+        
+    def test_save_method(self):
+        self.Portrait.save_tag()
+        tag = tags.objects.all()
+        self.assertTrue(len(tag)>0)    
+        
+    def test_delete_method(self):
+        self.Portrait.delete_tag()
+        tag = tags.objects.all()
+        self.assertTrue(len(tag)==0)
